d582f0ba6cdc11966deb421e6a3cf4a5b0edab54
Manthan Raut <manthanr141@gmail.com>
2026-01-24 16:29:52 +0530

    change in app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-vercel-app.vercel.app"
+    "https://gdgoc-website-six.vercel.app/"
   ],
   credentials: true
 }));
